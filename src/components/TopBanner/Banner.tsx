dbc9989a0390f867c098f0b5c68e74df33794f7f
Minsung Kim <epicblue@hanmail.net>
2022-01-19 16:35:10 +0900

    1200px 이상 화면 무한 스크롤 구현 완료

--- src/components/TopBanner/Banner.tsx
+++ src/components/TopBanner/Banner.tsx
@@ -2,19 +2,21 @@
 export interface BannerProps {
   src: string;
   title: string;
-  contents: string
+  contents: string;
+  current: number;
+  index: number
 }
 
-export const Banner: React.FC<BannerProps> = ({ src, title, contents }) => {
+export const Banner: React.FC<BannerProps> = ({ src, title, contents, current, index }) => {
   return (<div className="banner" >
     <img src={src} alt={title} />
-    <div className="content-box">
+    {(index + 1) % 6 === current % 6 && <div className="content-box">
       <div className='content-wrapper'>
         <div className="title">{title}</div>
         <div className="contents">{contents}</div>
 
       </div>
       <div className="link"><div>바로 가기 &gt;</div></div>
-    </div>
+    </div>}
   </div>);
 }
--- src/components/TopBanner/index.tsx
+++ src/components/TopBanner/index.tsx
@@ -1,12 +1,12 @@
-import React, { MutableRefObject, useRef, useState } from 'react'
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react'
 import Arrow from './Arrow'
-import { Banner, BannerProps } from './Banner'
+import { Banner, } from './Banner'
 import "./index.scss"
 
 // Loop 돌려서 얻을 데이터 작성
 // 실시간으로 UI 크기 구하기
 
-const banners: BannerProps[] = []
+const banners: { title: string, contents: string, src: string }[] = []
 
 for (let i = 0; i <= 5; i++) {
   banners.push({
@@ -32,7 +32,9 @@ const TopBanner = () => {
   // 이전 translateX를 보관하고 싶다.
   const [currImg, setCurrImg] = useState(1);
   const [resizer, setResizer] = useState(1);
+  const [transition, setTransition] = useState('transform 0.2s');
 
+  const docWidth = document.documentElement.offsetWidth;
   const onMouseDown: React.MouseEventHandler<HTMLDivElement> = (e) => {
     const slider = e.currentTarget;
     const mouseX = e.clientX;
@@ -41,16 +43,32 @@ const TopBanner = () => {
     const onMouseMove: Parameters<typeof slider.addEventListener>[1] = (e) => {
 
       function onMouseUp() {
+        const moveDistance = (e as MouseEvent).clientX - mouseX
+        if (moveDistance > docWidth * 0.3) {
+          setCurrImg(currImg - 1)
+        } else if (moveDistance < -docWidth * 0.3) {
+          setCurrImg(currImg + 1)
+        } else {
+
+          slider.style.transform = `translateX(${currentX}px)`
+        }
 
         slider.removeEventListener('mousemove', onMouseMove);
-        slider.style.transform = `translateX(${currentX + (e as MouseEvent).clientX - mouseX}px)`
         slider.removeEventListener('mouseup', onMouseUp);
         slider.removeEventListener('mouseleave', onMouseleave)
       }
 
       function onMouseleave() {
+        const moveDistance = (e as MouseEvent).clientX - mouseX
+        if (moveDistance > docWidth * 0.3) {
+          setCurrImg(currImg - 1)
+        } else if (moveDistance < -docWidth * 0.3) {
+          setCurrImg(currImg + 1)
+        } else {
+
+          slider.style.transform = `translateX(${currentX}px)`
+        }
         slider.removeEventListener('mousemove', onMouseMove);
-        slider.style.transform = `translateX(${currentX + (e as MouseEvent).clientX - mouseX}px)`
         slider.removeEventListener('mouseup', onMouseUp);
         slider.removeEventListener('mouseleave', onMouseleave)
 
@@ -58,7 +76,7 @@ const TopBanner = () => {
 
 
       slider.style.transform = `translateX(${currentX + (e as MouseEvent).clientX - mouseX}px)`
-      console.log(slider.style.transform);
+
       slider.addEventListener('mouseup', onMouseUp)
       slider.addEventListener('mouseleave', onMouseleave)
 
@@ -68,20 +86,51 @@ const TopBanner = () => {
 
   }
 
-  const isExtra: boolean = currImg <= 2 || currImg > banners.length - 1
+  useEffect(() => {
 
-  const docWidth = document.documentElement.offsetWidth;
+    if (currImg === 7) {
+      setTimeout(res => {
+        setTransition("none");
+        setCurrImg(currImg - 6);
+      }, 300)
+      return;
+    }
+
+    if (currImg === 0) {
+      setTimeout(res => {
+        setTransition("none");
+        setCurrImg(currImg + 6);
+      }, 300)
+      return;
+    }
+    if (transition !== "transform 0.2s")
+      new Promise((res) => {
+        setTransition('transform 0.2s');
+        return;
+      })
+  }, [transition, currImg])
+
 
   const sliderRef = useRef() as MutableRefObject<HTMLDivElement>
 
   const onArrowClick = (direction: "left" | "right") => {
 
 
-    const slider = sliderRef.current;
-    const currentX = slider.style.transform.match(/\d+/) ? +(slider.style.transform.match(/-{0,1}\d+/) as string[])[0] : 0;
-    const docWidth = 1020 + 30;
-    slider.style.transform = `translateX(${direction === "right" ? currentX - docWidth : currentX + docWidth}px)`
-    setCurrImg(currImg + direction === "right" ? 1 : -1)
+
+
+    setCurrImg((currImage) => {
+      if (direction === "left") {
+
+
+        return currImage - 1;
+
+      } else {
+
+
+        return currImage + 1;
+
+      }
+    })
   }
   const medianWidth = docWidth / 2;
   const initialX = medianWidth - (1050 / 2);
@@ -94,10 +143,12 @@ const TopBanner = () => {
   window.addEventListener('resize', onResize)
   return (
     <>
-      <div className='slider' onMouseDown={onMouseDown} style={{ transform: `translateX(${initialX - 1050 * banners.length}px )`, }} ref={sliderRef} >
+      <div className='slider' onMouseDown={onMouseDown} style={{ transform: `translateX(${initialX - 1050 * (currImg + 5)}px )`, transition }} ref={sliderRef} >
 
-        {isExtra && banners.map(data => (<Banner {...data} key={data.title} />))}
-        {banners.map(data => (<Banner {...data} key={data.title} />))}
+        {banners.map((data, index) => (<Banner {...data} key={data.title} index={index} current={currImg} />))}
+        {banners.map((data, index) => (<Banner {...data} key={data.title} index={index} current={currImg} />))}
+        {banners.map((data, index) => (<Banner {...data} key={data.title} index={index} current={currImg} />))}
+
       </div>
       <Arrow direction='left' onClick={onArrowClick} />
       <Arrow direction='right' onClick={onArrowClick} />
