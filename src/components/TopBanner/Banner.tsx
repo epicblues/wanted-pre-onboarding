737437bf951a5c0a85ab481dacc6b7468b1cce15
Minsung Kim <epicblue@hanmail.net>
2022-01-20 20:40:14 +0900

    opacity animation 효과 부여

--- src/components/TopBanner/Banner.tsx
+++ src/components/TopBanner/Banner.tsx
@@ -10,13 +10,13 @@ export interface BannerProps {
 export const Banner: React.FC<BannerProps> = ({ src, title, contents, current, index }) => {
   return (<div className="banner" >
     <img src={src} alt={title} />
-    {(index + 1) % 6 === current % 6 && <div className="content-box">
+    <div className="content-box" style={{ opacity: (index + 1) % 6 === current % 6 ? 1 : 0 }}>
       <div className='content-wrapper'>
         <div className="title">{title}</div>
         <div className="contents">{contents}</div>
 
       </div>
       <div className="link"><div>바로 가기 &gt;</div></div>
-    </div>}
+    </div>
   </div>);
 }
